430beda82457407944ac14eac8ed8e469865c298
Vincentyua <34027917+Vincentyua@users.noreply.github.com>
2019-04-13 16:01:20 +0800

    Create 二叉树的深度.py

---
+++ 剑指offer/二叉树的深度.py
@@ -0,0 +1,17 @@
+"""
+题目：输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+解题思路：使用递归的思想，树的深度是左子树和右子树最大的子树深度加一
+"""
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def TreeDepth(self, pRoot):
+        if not pRoot:
+            return 0
+        left = self.TreeDepth(pRoot.left)
+        right = self.TreeDepth(pRoot.right)
+        return max(left,right)+1
